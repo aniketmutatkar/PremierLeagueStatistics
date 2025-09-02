10da05ba78f704e420fe038a7101dcdb6aceb2f5
Aniket Mutatkar <8129438+aniketmutatkar@users.noreply.github.com>
2025-09-02 15:23:19 -0700

    cleaning up project to start with updated and correct logic - removing extra and uncessary code;

--- src/scrapers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Scrapers package initialization
-"""
-from .base_scraper import BaseScraper
-from .stats_scraper import StatsScraper
-from .multi_stats_scraper import MultiStatsScraper
-from .fixture_scraper import FixtureScraper
-
-__all__ = ['BaseScraper', 'StatsScraper', 'MultiStatsScraper', 'FixtureScraper']
